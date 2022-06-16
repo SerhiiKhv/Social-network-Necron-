021ae2ba9207489cfbe92b53fc7e55a26c92400a
Alamay <6939715@stud.nau.edu.ua>
2022-06-16 17:26:26 +0300

    FormControl refactoring

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -3,6 +3,8 @@ import {Form, Field} from 'react-final-form'
 import {connect} from "react-redux";
 import {login, logout} from "../../Redux/authMe-reducer";
 import {Redirect} from "react-router-dom";
+import {required} from "../../utils/validator/validator";
+import {Input} from "../common/FormController/FormController";
 
 
 const LoginForm = (props) => {
@@ -14,10 +16,12 @@ const LoginForm = (props) => {
         {({ handleSubmit, pristine, form, submitting }) => (
             <form onSubmit={handleSubmit}>
                 <div>
-                    <Field placeholder={"Email"} name={"email"} component={"input"}/>
+                    <Field placeholder={"Email"} name={"email"} component={Input}
+                           validate={required}/>
                 </div>
                 <div>
-                    <Field placeholder={"Password"} name={"password"} component={"input"} type={"password"}/>
+                    <Field placeholder={"Password"} name={"password"} component={Input}
+                           type={"password"} validate={required}/>
                 </div>
                 <div>
                     <Field type={"checkbox"} name={"rememberMe"} component={"input"}/> remember me
