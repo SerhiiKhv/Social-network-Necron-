cff4f971741607d6bdb68950f6eae74ac42e44d6
Alamay <khvechuksergiyko@gmail.com>
2023-01-24 23:06:57 +0200

    WebSocket Update for Chat

--- src/Component/Content/Chat/ChatPage.tsx
+++ src/Component/Content/Chat/ChatPage.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 
-const wsChanel = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
 const ChatPage: React.FC = () => {
     return (
         <div>
@@ -10,10 +9,35 @@ const ChatPage: React.FC = () => {
 }
 
 const Chat: React.FC = () => {
+    const [wsChanel, SetWsChanel] = useState<WebSocket | null>(null)
+
+    useEffect(() => {
+        let ws: WebSocket
+        const closeHandler = () => {
+            console.log('CLOSE WS')
+            setTimeout(createChanel, 3000)
+        }
+
+        function createChanel() {
+            ws?.removeEventListener('close', closeHandler)
+            ws?.close()
+            ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+            ws.addEventListener('close', closeHandler)
+            SetWsChanel(ws)
+        }
+
+        createChanel()
+
+        return () => {
+            ws.removeEventListener('close', closeHandler)
+            ws.close()
+        }
+    }, [])
+
     return (
         <div>
-            <Messages/>
-            <AddMessage/>
+            <Messages wsChanel={wsChanel}/>
+            <AddMessage wsChanel={wsChanel}/>
         </div>
     )
 }
@@ -25,15 +49,22 @@ type ChatMessageType = {
     userName: string
 }
 
-const Messages: React.FC = () => {
+const Messages: React.FC<{wsChanel: WebSocket | null}> = ({wsChanel}) => {
     const [messages, setMessages] = useState<ChatMessageType[]>([])
 
     useEffect(() => {
-        wsChanel.addEventListener('message', (e: MessageEvent) => {
+
+        let messageHandler = (e: MessageEvent) => {
             let newMessage = JSON.parse(e.data)
             setMessages((prevMessages) => [...prevMessages, ...newMessage])
-        })
-    }, [])
+        }
+
+        wsChanel?.addEventListener('message', messageHandler)
+
+        return () => {
+            wsChanel?.addEventListener('message', messageHandler)
+        }
+    }, [wsChanel])
 
     return (
         <div style={{height: '500px', overflow: 'auto'}}>
@@ -43,9 +74,15 @@ const Messages: React.FC = () => {
 }
 
 const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
+
+    let CheckPhotoMessage = (photo: string) => {
+        return photo !== null? photo : 'https://cdn1.vectorstock.com/i/1000x1000/15/25/approved-person-icon-male-add-user-person-avatar-vector-21201525.jpg'
+    }
+
+
     return (
         <div>
-            <img src={message.photo} style={{height: '45px'}}/>
+            <img src={CheckPhotoMessage(message.photo)} style={{height: '45px'}}/>
             <b> {message.message} </b>
             <b> {message.userName} </b>
             <hr/>
@@ -54,24 +91,36 @@ const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
 }
 
 
-const AddMessage: React.FC = () => {
+const AddMessage: React.FC<{wsChanel: WebSocket | null}> = ({wsChanel}) => {
     const [messages, setMessages] = useState('')
+    const [readyStatus, setReadyStatus] = useState<'pending' | 'ready'>('pending')
+
+
+    useEffect(() => {
+        let openHandler = () => {
+            setReadyStatus('ready')
+        }
+        wsChanel?.addEventListener('open', openHandler)
+
+        return () => {
+            wsChanel?.addEventListener('open', openHandler)
+        }
+    },[wsChanel])
 
     const sendMessages = () => {
         if (!messages) {
             return
         }
-        wsChanel.send(messages)
+        wsChanel?.send(messages)
         setMessages('')
     }
 
     return (
         <div>
             <textarea value={messages} onChange={(e) => setMessages(e.currentTarget.value)}></textarea>
-            <button onClick={sendMessages}>Submit</button>
+            <button disabled={wsChanel === null || readyStatus !== 'ready'} onClick={sendMessages}>Submit</button>
         </div>
     )
 }
-
 
 export default ChatPage
