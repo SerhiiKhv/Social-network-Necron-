1c6fc046e8a42aabd5f7aa1c53c8c9c3c0b3aaec
Alamay <khvechuksergiyko@gmail.com>
2023-03-16 16:56:41 +0200

    Added the Japanese language

---
+++ src/Language/LanguageType/jp.ts
@@ -0,0 +1,40 @@
+export const JapanLanguage = {
+    navMenu: {
+        profile: "プロフィール",
+        setting: "設定",
+        music: "音楽",
+        users: "ユーザー",
+        chat: "チャット"
+    },
+    loginLanguage: {
+        login: "ログイン",
+        loginButton: "ログイン"
+    },
+    headerLanguage: {
+        login: "ログイン",
+        logout: "ログアウト"
+    },
+    settingLanguage: {
+        setting: "設定",
+        avatar: "あなたのアバター",
+        status: "あなたのステータス"
+    },
+    musicLanguage: {
+        music: "音楽"
+    },
+    usersSearchLanguage: {
+        follow: "フォロー中",
+        unFollow: "フォローする",
+        all: "すべて",
+        onlyFollowed: "フォローしているユーザーのみ",
+        onlyUnfollowed: "フォローしていないユーザーのみ",
+        submit: "検索"
+    },
+    paginatorLanguage: {
+        next: "次へ",
+        back: "前へ"
+    },
+    chatLanguage: {
+        submit: "送信"
+    }
+}
--- src/Language/components/LanguageSwitcher.tsx
+++ src/Language/components/LanguageSwitcher.tsx
@@ -1,7 +1,8 @@
 import React, {useContext} from "react";
 import {LanguageContext} from "./LanguageContext";
-import UKFlag from "../img/UKFlag.jpg";
+import UKFlag from "../img/UAFlag.jpg";
 import ENFlag from "../img/ENFlag.png";
+import JPFlag from "../img/JapanFlag.gif";
 import style from "./LanguageSwitcher.module.scss"
 
 const LanguageSwitcher: React.FC = () => {
@@ -10,13 +11,23 @@ const LanguageSwitcher: React.FC = () => {
         setLang(event.target.value);
     };
 
+    const CheckFlag = () => {
+        if( lang == "jp"){
+            return <img src={JPFlag} className={style.imgFlag} alt={"Loading..."}/>
+        }else if ( lang == "en"){
+            return <img src={ENFlag} className={style.imgFlag} alt={"Loading..."}/>
+        }else {
+            return <img src={UKFlag} className={style.imgFlag} alt={"Loading..."}/>
+        }
+    }
+
     return (
         <div>
-            {lang == "uk"? <img src={UKFlag} className={style.imgFlag}/> :
-                <img src={ENFlag} className={style.imgFlag}/>}
+            {CheckFlag()}
             <select value={lang} onChange={handleChangeLang} className={style.select}>
                     <option value="uk" className={style.option}> Українська</option>
                     <option value="en" className={style.option}> English</option>
+                    <option value="jp" className={style.option}> Japan</option>
             </select>
         </div>
     );
